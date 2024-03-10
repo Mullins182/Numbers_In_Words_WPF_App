b73c971446de27c8e9cfd3b417b623fc9dbb5ed3
Mullins182 <Mullins182@t-online.de>
2024-03-10 20:33:42 +0100

    merge conflict test

--- Numbers_In_Words_WPF_App/MainWindow.xaml.cs
+++ Numbers_In_Words_WPF_App/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Numbers_In_Words_WPF_App.Classes;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;                   // Dies ist der Dev BRanch Commit
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
